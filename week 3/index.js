5b6dde1ea794fe08925dd6a856ee0359ecd4a849
dipkamalbomjan-cmyk <dipkamalbomjan@gmail.com>
2025-10-16 16:03:07 +0000

    new changes

--- week 3/index.js
+++ week 3/index.js
@@ -1,1 +1,12 @@
+// Definition of the function EmployeeInfo
+function EmployeeInfo(name, Salary) {
+    console.log("Welcome " + name + ", your monthly salary is " + Salary);
+}
 
+console.log("This is my first program");
+
+var EmpName = "John";
+var EmpSalary = 50000;
+
+// Calling the function EmployeeInfo
+EmployeeInfo(EmpName, EmpSalary);
